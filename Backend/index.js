156218544b7839ed56ce9c9605754809c98e4144
29!@#vansthaliA <chait8126@gmail.com>
2024-02-11 01:32:17 +0530

    login

--- Backend/index.js
+++ Backend/index.js
@@ -4,11 +4,12 @@ const { default: mongoose } = require('mongoose');
 const UserModel = require('./models/User');
 const bcrypt = require('bcryptjs');
 const salt = bcrypt.genSaltSync(10);
-
+const jwt = require("jsonwebtoken");
 const app = express();
+let secret = "fjhgjdi83948479hkrshkshgh948y";
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ credentials: true, origin: 'http://localhost:5173' }));
 
 mongoose.connect('mongodb+srv://chait8126:29vansthaliA@cluster0.vsimsnr.mongodb.net/');
 
@@ -21,6 +22,27 @@ app.post('/register', async (req, res) => {
         res.json(user);
     } catch (e) {
         res.status(400).json({ msg: "user already exists", excc: e });
+    }
+});
+
+app.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        let user = await UserModel.findOne({ username });
+        if (user === null) {
+            res.status(404).json({ msg: "Username is not correct" });
+        } else {
+            let password_check = bcrypt.compareSync(password, user.password);
+            if (!password_check) {
+                res.status(404).json({ msg: "Username and password combination is not correct" });
+            }
+            let id = user._id;
+            let token = await jwt.sign({ username, id }, secret);
+            res.status(200).cookie("token", token).json('ok');
+        }
+    } catch (e) {
+        console.log(e);
+        res.status(404).json(e);
     }
 });
 
--- Frontend/MernBlogFrontend/src/pages/LoginPage.jsx
+++ Frontend/MernBlogFrontend/src/pages/LoginPage.jsx
@@ -1,12 +1,49 @@
-import React from 'react'
+import { useState } from 'react';
+import { Navigate } from 'react-router-dom';
 
 export const LoginPage = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [redirect, setRedirect] = useState(false);
+    async function login(e) {
+        e.preventDefault();
+        console.log(JSON.stringify({ username, password }));
+        let response = await fetch("http://localhost:3000/login", {
+            method: "POST",
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ username, password }),
+            credentials: 'include',
+        });
+        if (response.ok) {
+            setRedirect(true);
+        } else {
+            alert('wrong credentials');
+        }
+    }
+
+    if (redirect) {
+        return (<Navigate to={'/'}></Navigate>);
+    }
     return (
         <div>
-            <form className='register'>
+            <form className='login' onSubmit={login}>
                 <h1>Login</h1>
-                <input type='text' placeholder='Username'></input>
-                <input type='password' placeholder='Password'></input>
+                <input
+                    type='text'
+                    placeholder='Username'
+                    value={username}
+                    onChange={(e) => { setUsername(e.target.value) }}
+                ></input>
+                <input
+                    type='password'
+                    placeholder='Password'
+                    value={password}
+                    onChange={(e) => { setPassword(e.target.value) }}
+                ></input>
                 <button >Login</button>
             </form>
         </div>
